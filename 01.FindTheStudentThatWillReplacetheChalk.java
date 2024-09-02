77aeb9aab030f2bf78ee1d636702dd060158e7ff
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-09-02 09:36:58 +0530

    Update 01.FindTheStudentThatWillReplacetheChalk.java

--- 01.FindTheStudentThatWillReplacetheChalk.java
+++ 01.FindTheStudentThatWillReplacetheChalk.java
@@ -1,14 +1,15 @@
 /*
-1894 - Find the Student that Will Replace the Chalk
+URL: https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/description/
 
-URL - https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/description/
+1894. Find the Student that Will Replace the Chalk
 
 There are n students in a class numbered from 0 to n - 1. 
-The teacher will give each student a problem starting with the student number 0, then the student number 1, and so on until the teacher reaches the student number n - 1.
+The teacher will give each student a problem starting with the student number 0, then the student number 1, and so on until the teacher reaches the student number n - 1. 
 After that, the teacher will restart the process, starting with the student number 0 again.
 
 You are given a 0-indexed integer array chalk and an integer k. 
-There are initially k pieces of chalk. When the student number i is given a problem to solve, they will use chalk[i] pieces of chalk to solve that problem. 
+There are initially k pieces of chalk. 
+When the student number i is given a problem to solve, they will use chalk[i] pieces of chalk to solve that problem. 
 However, if the current number of chalk pieces is strictly less than chalk[i], then the student number i will be asked to replace the chalk.
 
 Return the index of the student that will replace the chalk pieces.
@@ -39,6 +40,12 @@ Explanation: The students go in turns as follows:
 - Student number 3 uses 2 chalk so k = 5.
 - Student number 0 uses 3 chalk so k = 2.
 Student number 1 does not have enough chalk, so they will have to replace it.
+
+Constraints:
+	chalk.length == n
+	1 <= n <= 105
+	1 <= chalk[i] <= 105
+	1 <= k <= 109
 
 */
 // Brute force --- TC - O(n) ---- SC - O(1)
