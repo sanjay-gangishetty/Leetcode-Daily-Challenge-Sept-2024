8026c85a8b23736cf3f2fc91bd9c2b6ac55a62b5
Sanjay <102904314+sanjay-gangishetty@users.noreply.github.com>
2024-09-12 22:05:14 +0530

    Update 12.CountNumberOfConsistentStrings.java

--- 12.CountNumberOfConsistentStrings.java
+++ 12.CountNumberOfConsistentStrings.java
@@ -1,10 +1,12 @@
 /*
-URL - https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+URL: https://leetcode.com/problems/count-the-number-of-consistent-strings/description/?envType=daily-question&envId=2024-09-12
+
 1684. Count the Number of Consistent Strings
 
 You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
 Return the number of consistent strings in the array words.
 
+ 
 Example 1:
 Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
 Output: 2
@@ -19,13 +21,13 @@ Example 3:
 Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
 Output: 4
 Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
- 
+
 Constraints:
-1 <= words.length <= 104
-1 <= allowed.length <= 26
-1 <= words[i].length <= 10
-The characters in allowed are distinct.
-words[i] and allowed contain only lowercase English letters.
+	1 <= words.length <= 104
+	1 <= allowed.length <= 26
+	1 <= words[i].length <= 10
+	The characters in allowed are distinct.
+	words[i] and allowed contain only lowercase English letters.
 */
 // Brute force Approach ----- TC - O(n^3) ------ SC - O(n)
 class Solution {
